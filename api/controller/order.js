2799b11347cf394c32ee916b0b1b1a1cc265dc89
vishal kaira <kairavishal37@gmail.com>
2023-07-05 20:18:36 +0300

    .

--- api/controller/order.js
+++ api/controller/order.js
@@ -41,9 +41,9 @@ exports.intent = async(req, res, next) => {
         })
         await neworder.save();
         console.log("order confirmed")
-        res.status(200).send({
-            clientSecret: paymentIntent.client_secret,
-          });
+        // res.status(200).send({
+        //     clientSecret: paymentIntent.client_secret,
+        //   });
 
     }catch(err){
 
