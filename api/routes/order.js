b74a74fdaa667bfbed6f0c1bff31b6fd2b0ce65e
vishal kaira <kairavishal37@gmail.com>
2023-07-05 19:46:51 +0300

    .

--- api/routes/order.js
+++ api/routes/order.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const {getOrders,intent,confirm } = require("../controller/order.js");
 const{verifytoken}=require("../middleware/jwt.js")
 router.get("/",verifytoken,getOrders)
-router.post("create-payment-intent/:id",verifytoken,intent)//taking gig id to get price.
+router.post("/create-payment-intent/:id",verifytoken,intent)//taking gig id to get price.
 router.put("/",verifytoken,confirm)
 
 module.exports = router;
