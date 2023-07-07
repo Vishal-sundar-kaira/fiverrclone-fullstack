03997d914699a954b00510a91f783303088b2a1d
vishal kaira <kairavishal37@gmail.com>
2023-07-07 11:54:41 +0300

    .

--- Client/src/Pages/checkoutform/CheckoutForm.jsx
+++ Client/src/Pages/checkoutform/CheckoutForm.jsx
@@ -59,7 +59,7 @@ const CheckoutForm = () => {
           elements,
           confirmParams: {
             // Make sure to change this to your payment completion page
-            return_url: "/success",
+            return_url: returnUrl,
           },
         });
     
