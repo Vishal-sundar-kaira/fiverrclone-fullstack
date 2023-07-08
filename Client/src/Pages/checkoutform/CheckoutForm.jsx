cadb8a90100fe4cc4846b0604471cd4a4d6ae72e
vishal kaira <kairavishal37@gmail.com>
2023-07-08 18:22:15 +0300

    made changes in return_url creating async problem.

--- Client/src/Pages/checkoutform/CheckoutForm.jsx
+++ Client/src/Pages/checkoutform/CheckoutForm.jsx
@@ -60,7 +60,7 @@ const CheckoutForm = () => {
           elements,
           confirmParams: {
             // Make sure to change this to your payment completion page
-            return_url: returnUrl,
+            return_url: process.env.REACT_APP_BASE_URL + "/success",
           },
         });
     
